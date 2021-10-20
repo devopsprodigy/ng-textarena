f3f1b03ad24e192af8e0382f749536cfc6caa75e
Kuznetsova <duxwife@yandex.ru>
2021-10-20 15:09:43 +0800

    if disable changes set editable

--- projects/angular-textarena/src/lib/formable-textarena/formable-textarena.component.ts
+++ projects/angular-textarena/src/lib/formable-textarena/formable-textarena.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef, forwardRef, Optional, Host, SkipSelf} from '@angular/core';
+import {Component, OnInit, Input, ViewChild, ElementRef, forwardRef, Optional, Host, SkipSelf, OnChanges, SimpleChanges} from '@angular/core';
 import Textarena from '@itsumma/textarena';
 import TextarenaOptions from '@itsumma/textarena/lib/interfaces/TextarenaOptions';
 import TextarenaData from '@itsumma/textarena/lib/interfaces/TextarenaData';
-import {AbstractControl, ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {AngularTextarenaService} from '../angular-textarena.service';
 @Component({
   selector: 'ng-textarena-formable',
@@ -19,7 +19,7 @@ import {AngularTextarenaService} from '../angular-textarena.service';
     },
   ],
 })
-export class AngularTextarenaFormableComponent implements OnInit, ControlValueAccessor {
+export class AngularTextarenaFormableComponent implements OnInit, ControlValueAccessor, OnChanges {
   @Input() settings: TextarenaOptions | undefined;
   @Input() formControlName: string;
   @Input() disabled?: boolean;
@@ -50,6 +50,12 @@ export class AngularTextarenaFormableComponent implements OnInit, ControlValueAc
       initData,
       ...options,
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.disabled && this.disabled !== undefined && this.textArena) {
+      this.textArena.setEditable(!this.disabled);
+    }
   }
 
   registerOnChange(fn: any) {
