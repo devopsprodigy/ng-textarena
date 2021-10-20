a028ffd0828c6e11f19bc69b9cb4b15469db5ca0
exsmund <exsmund@gmail.com>
2021-10-20 16:17:25 +0800

    Release package 0.0.14

--- projects/angular-textarena/src/lib/angular-textarena.service.ts
+++ projects/angular-textarena/src/lib/angular-textarena.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { defaultOptions } from '@itsumma/textarena';
 import TextarenaOptions from '@itsumma/textarena/lib/interfaces/TextarenaOptions';
-import dataHtml from './default/dataHtml';
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +10,6 @@ export class AngularTextarenaService {
   constructor() { }
 
   createOptions(settings: TextarenaOptions): TextarenaOptions | undefined {
-    if (!settings) {
-      return defaultOptions;
-    }
-    const obj: TextarenaOptions = {};
-    obj.initData = settings?.initData || {dataHtml};
-    obj.debug = settings?.debug || false;
-    obj.editable = settings?.editable || true;
-    if (settings?.plugins?.length){
-      obj.plugins = settings.plugins;
-    }
-    obj.creatorBar = settings?.creatorBar || undefined;
-    obj.toolbar = settings?.toolbar || undefined;
-    return obj;
+    return { ...defaultOptions, ...(settings || {})};
   }
 }
